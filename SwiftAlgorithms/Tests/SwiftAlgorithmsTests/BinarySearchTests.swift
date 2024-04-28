4f6c9e8cfd60c12f8a967673fec027dbc9e0ff88
Saurabh Verma <saurabh1088@gmail.com>
2024-04-28 22:50:43 +0530

    Added some unit tests for string data type.

--- SwiftAlgorithms/Tests/SwiftAlgorithmsTests/BinarySearchTests.swift
+++ SwiftAlgorithms/Tests/SwiftAlgorithmsTests/BinarySearchTests.swift
@@ -9,49 +9,114 @@ import XCTest
 @testable import SwiftAlgorithms
 
 final class BinarySearchTests: XCTestCase {
+    
+    private let intArray = [1, 
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9,
+                            10]
+    private let stringArray = ["A", 
+                               "B",
+                               "C",
+                               "D",
+                               "E", 
+                               "F",
+                               "G",
+                               "H", 
+                               "I",
+                               "J",
+                               "K",
+                               "L",
+                               "M",
+                               "N",
+                               "O",
+                               "P",
+                               "Q",
+                               "R",
+                               "S",
+                               "T",
+                               "U",
+                               "V",
+                               "W",
+                               "X",
+                               "Y",
+                               "Z"]
 
-    func test_binarySearch_success_1() {
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let index = BinarySearch.searchFor(value: 2, in: array)
+    // MARK: -----------------------------------------------------------------------
+    // MARK: Binary search tests for Int data type
+    func test_binarySearchInt_success_1() {
+        let index = BinarySearch.searchFor(value: 2, in: intArray)
         XCTAssertEqual(1, index)
     }
     
-    func test_binarySearch_success_2() {
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let index = BinarySearch.searchFor(value: 8, in: array)
+    func test_binarySearchInt_success_2() {
+        let index = BinarySearch.searchFor(value: 8, in: intArray)
         XCTAssertEqual(7, index)
     }
     
-    func test_binarySearch_fail() {
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let index = BinarySearch.searchFor(value: 0, in: array)
+    func test_binarySearchInt_fail() {
+        let index = BinarySearch.searchFor(value: 0, in: intArray)
         XCTAssertNil(index)
     }
     
-    func test_binarySearch_hugeDataSet() {
+    func test_binarySearchInt_hugeDataSet() {
         let arraySize = 10000
         let array = (0..<arraySize).map { $0 }
         let index = BinarySearch.searchFor(value: 999, in: array)
         XCTAssertEqual(999, index)
     }
     
-    func test_binarySearchRecursive_success_1() {
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let index = BinarySearch.searchRecursivelyFor(value: 1, in: array)
+    func test_binarySearchRecursiveInt_success_1() {
+        let index = BinarySearch.searchRecursivelyFor(value: 1, in: intArray)
         XCTAssertEqual(0, index)
     }
     
-    func test_binarySearchRecursive_success_2() {
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let index = BinarySearch.searchRecursivelyFor(value: 9, in: array)
+    func test_binarySearchRecursiveInt_success_2() {
+        let index = BinarySearch.searchRecursivelyFor(value: 9, in: intArray)
         XCTAssertEqual(8, index)
     }
     
-    func test_binarySearchRecursive_fail() {
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let index = BinarySearch.searchRecursivelyFor(value: 0, in: array)
+    func test_binarySearchRecursiveInt_fail() {
+        let index = BinarySearch.searchRecursivelyFor(value: 0, in: intArray)
         XCTAssertNil(index)
     }
+    
+    // MARK: -----------------------------------------------------------------------
+    // MARK: Binary search tests for String data type
+    func test_binarySearchString_success_1() {
+        let index = BinarySearch.searchFor(value: "C", in: stringArray)
+        XCTAssertEqual(2, index)
+    }
+    
+    func test_binarySearchString_success_2() {
+        let index = BinarySearch.searchFor(value: "X", in: stringArray)
+        XCTAssertEqual(23, index)
+    }
+    
+//    func test_binarySearchString_fail() {
+//        let index = BinarySearch.searchFor(value: "QW", in: stringArray)
+//        XCTAssertNil(index)
+//    }
+    
+    func test_binarySearchRecursiveString_success_1() {
+        let index = BinarySearch.searchRecursivelyFor(value: "C", in: stringArray)
+        XCTAssertEqual(2, index)
+    }
+    
+    func test_binarySearchRecursiveString_success_2() {
+        let index = BinarySearch.searchRecursivelyFor(value: "X", in: stringArray)
+        XCTAssertEqual(23, index)
+    }
+    
+//    func test_binarySearchRecursiveString_fail() {
+//        let index = BinarySearch.searchRecursivelyFor(value: "0", in: stringArray)
+//        XCTAssertNil(index)
+//    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
